65a919696d5bbf3664e223b801795d01668ddbc0
Andres Camilo Castro Orozoco <ac.castro18@ciaf.edu.co>
2024-11-11 20:06:31 -0500

    Header.tsx

--- frontend/src/Header/Header.tsx
+++ frontend/src/Header/Header.tsx
@@ -3,7 +3,17 @@ import "./Header.css";
 function Header() {
   return (
     <header className="header">
-      <div></div>
+      <div className="logo">
+        <img src="../../img/logo.png" alt="Logo" width="50" height="50" />
+      </div>
+      <nav className="navbar">
+        <ul className="navbar-menu">
+          <li className="navbar-item"><a href="#home">Home</a></li>
+          <li className="navbar-item"><a href="#about">About</a></li>
+          <li className="navbar-item"><a href="#services">Services</a></li>
+          <li className="navbar-item"><a href="#contact">Contact</a></li>
+        </ul>
+      </nav>
     </header>
   );
 }
