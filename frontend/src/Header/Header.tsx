e43762a472542cfe5aee527fe5898a4da30a956d
Andres Camilo Castro Orozoco <ac.castro18@ciaf.edu.co>
2024-11-11 20:25:56 -0500

    Header.tsx

--- frontend/src/Header/Header.tsx
+++ frontend/src/Header/Header.tsx
@@ -8,10 +8,10 @@ function Header() {
       </div>
       <nav className="navbar">
         <ul className="navbar-menu">
-          <li className="navbar-item"><a href="#home">Home</a></li>
-          <li className="navbar-item"><a href="#about">About</a></li>
-          <li className="navbar-item"><a href="#services">Services</a></li>
-          <li className="navbar-item"><a href="#contact">Contact</a></li>
+          <li className="navbar-item"><a href="#home">Inicio</a></li>
+          <li className="navbar-item"><a href="#about">Carrito</a></li>
+          <li className="navbar-item"><a href="#services">Servicios</a></li>
+          <li className="navbar-item"><a href="#contact">Contactenos</a></li>
         </ul>
       </nav>
     </header>
