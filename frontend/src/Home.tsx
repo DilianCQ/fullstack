a7a2525d30d6b95adbae71023c601da7e6670813
Andres Camilo Castro Orozoco <ac.castro18@ciaf.edu.co>
2024-11-22 03:00:21 +0000

    change impor style

--- frontend/src/Home.tsx
+++ frontend/src/Home.tsx
@@ -1,7 +1,7 @@
 import Axios from "axios";
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
-
+import "./home.css";
 function Home() {
   const [data, setData] = useState([]);
 
