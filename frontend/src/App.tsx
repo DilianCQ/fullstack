5c28277c976a81ac84003b4636a16bbd8145c2e6
carlosdanielgr <carlos.dgr.12@gmail.com>
2024-11-21 19:28:50 -0500

    Changes to root structure

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,28 +1,27 @@
-import { useState } from 'react'
-import { Route, BrowserRouter as Router, Routes } from 'react-router-dom'
+import { useState } from "react";
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 
-import './App.css'
-import CreateProducts from './createProducts'
-import Home from './Home'
-import Header from './Header/Header'
-import Footer from './Footer/Footer'
-
+import "./App.css";
+import CreateProducts from "./createProducts";
+import Home from "./Home";
+import Header from "./Header/Header";
+import Footer from "./Footer/Footer";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
-    <Header/>
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/create-product" element={<CreateProducts />} />
-      </Routes>
-    </Router>
-    <Footer/>
+      <Router>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/create-product" element={<CreateProducts />} />
+        </Routes>
+        <Footer />
+      </Router>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
