1006cb15ddd8744f6a30e09895143241961dce49
CamiloCastro118 <ac.castro18@gmail.com>
2024-11-23 10:00:33 -0500

    test rep_comit

--- frontend/src/Header/Header.tsx
+++ frontend/src/Header/Header.tsx
@@ -11,7 +11,7 @@ function Header() {
   return (
     <header className="header">
       <div className="logo">
-        <img src="public/logo.png" alt="Logo" width="50" height="50" />
+        <img src="./public/img/logo.png" alt="Logo" width="50" height="50" />
       </div>
       <nav className="navbar">
         <ul className="navbar-menu">
